4099aaabdba42637f5a57d22f86ea84cd5f7ec17
raveekiran <raveekiran@gmail.com>
2016-10-01 03:32:50 -0400

    Update rundrill.sh

--- rundrill.sh
+++ rundrill.sh
@@ -13,4 +13,5 @@ if [ ! -z "$2" ] ; then
 	sed -ie "s/localhost:2181/$2/" /apache-drill-1.8.0/conf/drill-override.conf
 fi;
 /apache-drill-1.8.0/bin/drillbit.sh --config /apache-drill-1.8.0/conf start
+touch /apache-drill-1.8.0/log/drillbit.out
 tail -f /apache-drill-1.8.0/log/drillbit.out
