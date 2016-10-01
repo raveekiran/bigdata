d916b32626f8b563eaa0e8f6233549c936f4c697
raveekiran <raveekiran@gmail.com>
2016-10-01 03:21:26 -0400

    Update rundrill.sh

--- rundrill.sh
+++ rundrill.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+echo " Drillbit on host $HOSTNAME is starting ... "
 
-sed -ie "s/export JAVA_HOME/#export JAVA_HOME/" apache-drill-1.8.0/bin/drill-config.sh
+sed -ie "s/export JAVA_HOME/#export JAVA_HOME/" /apache-drill-1.8.0/bin/drill-config.sh
 
 if [ ! -z "$1" ] ; then
-	sed -ie "s/drillbits1/$1/" apache-drill-1.8.0/conf/drill-override.conf
+	echo " Setting cluster name to $1... "
+	sed -ie "s/drillbits1/$1/" /apache-drill-1.8.0/conf/drill-override.conf
 fi;
 
 if [ ! -z "$2" ] ; then
-	sed -ie "s/localhost:2181/$2/" apache-drill-1.8.0/conf/drill-override.conf
+	echo " Setting ZK quorum  to $2... "
+	sed -ie "s/localhost:2181/$2/" /apache-drill-1.8.0/conf/drill-override.conf
 fi;
-apache-drill-1.8.0/bin/drillbit.sh --config apache-drill-1.8.0/conf start
-tail -f apache-drill-1.8.0/log/drillbit.out
+/apache-drill-1.8.0/bin/drillbit.sh --config /apache-drill-1.8.0/conf start
+tail -f /apache-drill-1.8.0/log/drillbit.out
